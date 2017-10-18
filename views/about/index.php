1a5a012fe6c02cef0aa4c284fb566e2bb8900aea
jeffrey <jeffrey@10.100.13.74>
2017-10-18 16:19:10 +0200

    About index page cleanup

--- views/about/index.php
+++ views/about/index.php
@@ -4,9 +4,9 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 
 echo $this->render('@humhub/modules/learn4dev/views/common/crumb');
+
+$path = $this->theme->getBaseUrl() . '/img/about/';
 $members = $this->theme->getBaseUrl() . '/img/members.jpg';
-
-
 ?>
 <div id="static">
     <div class="container">
@@ -18,17 +18,20 @@ $members = $this->theme->getBaseUrl() . '/img/members.jpg';
                     <strong>Learn4dev</strong> is an international network of development organisations
                     from different backgrounds. We work together to provide better 
                     learning opportunities for our staff and partners. 
-                    Donor collaboration enhances aid efficiency for poverty reduction.
                 </p>
-                <p>
-                    (read the learn4dev history – pdf file)
+                <p style="text-align:center">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/qPfJz1QNyrI" frameborder="0" allowfullscreen></iframe>
+                </p>
+
                 <p>
                     The network is not formalised nor has its own staff or secretariat.
-                    It exists thanks to the commitment of its members to contribute to 
-                    more efficient aid. It thereby benefits from its informal and thus
-                    flexible structure.
+                    It exists thanks to the commitment of its members to 
+                    contribute to more efficient aid. It thereby benefits from 
+                    its informal and thus flexible structure.
                 </p>
-                (read our charter – pdf file)
+                <p>
+                    <a href="<?= $path . 'learn4dev-charter-1706.pdf' ?>" download="filename">read our charter – pdf file</a> 
+                </p>
                 <p>
                     The essence of the learn4dev network are the Expert Groups. 
                     Each group is created around a specific topic or common area of 
@@ -37,15 +40,7 @@ $members = $this->theme->getBaseUrl() . '/img/members.jpg';
                     the co-creation of tools or the sharing of knowledge.
                 </p>   
                 <p>
-                    The general coordination of the network is done by the members of the Core Group.
-                    This troika includes the organisers of last year’s annual meeting,
-                    the current annual meeting and next year’s, the chair of the 
-                    network, the expert group leader and the communication team.
-                </p>
-                <p>
-                    Finally, once a year, all members of the network meet at the Annual 
-                    Meeting. It is a unique opportunity to gather experts and partners 
-                    to set together the strategic objectives of the network.
+                    <a href="<?= $path . 'learn4dev-history.pdf' ?>" download="filename">read our history – pdf file</a> 
                 </p>
                 <p>
                     Today the network counts 30 member organisations including bilateral
@@ -68,24 +63,9 @@ $members = $this->theme->getBaseUrl() . '/img/members.jpg';
                 </p>
 
             </div>
-            <?= \humhub\modules\learn4dev\widgets\ThumbnailWidget::widget(['displayLabels'=>false,'data' => $model->content['relatedPages'], 'sm' => 4, 'md' => 4, 'lg' => 4]); ?>
-
-
+            <?= \humhub\modules\learn4dev\widgets\ThumbnailWidget::widget(['displayLabels' => false, 'data' => $model->content['relatedPages'], 'sm' => 4, 'md' => 4, 'lg' => 4]); ?>
 
         </div>
     </div>
 </div>
 
-<div class="container">
-    <div class="row" id="history-video">
-        <div class="col-md-6" id="history-video-text">
-            <h4>
-                Want to find out more?
-            </h4>
-            Watch the video or visit our history.
-
-        </div>
-
-        <div class="col-md-6"><iframe width="560" height="315" src="https://www.youtube.com/embed/fustwYQdRBQ" frameborder="0" allowfullscreen></iframe></div>
-    </div>
-</div>
