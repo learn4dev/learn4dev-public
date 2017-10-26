6825131d049f0898d20a2ff0258e54d690aa7ed4
jeffrey <jeffrey@10.100.13.74>
2017-10-26 10:16:46 +0200

    Wired spaces for learn page

---
+++ static/about/network_members.php
@@ -0,0 +1,88 @@
+<?php
+
+use yii\helpers\Url;
+
+return [
+    'id' => 'network_members',
+    'container' => 'gallery',
+    [
+        'id' => 'agence-francaise-de-development',
+        'url' => 'https://www.afd.fr/en',
+    ],
+    [
+        'id' => 'australian-government-department-of-foreign-affairs-and-trade',
+        'url' => 'http://dfat.gov.au',
+    ],
+    [
+        'id' => 'australian-development-cooperation',
+        'url' => 'http://www.entwicklung.at/en/',
+    ],
+    [
+        'id' => 'belgian-development-agency',
+        'url' => 'https://www.btcctb.org/',
+    ],
+    [
+        'id' => 'belgian-development-cooperation',
+        'url' => 'https://diplomatie.belgium.be/en/policy/development_cooperation',
+    ],
+    [
+        'id' => 'bmz-federal-ministry-for-cooperation-and-development',
+        'url' => 'http://formin.finland.fi/public/default.aspx?culture=en-US&contentlan=2',
+    ],
+    [
+        'id' => 'canada-global-issues-and-international-assistance',
+        'url' => 'http://international.gc.ca/world-monde/issues_development-enjeux_developpement/index.aspx?lang=eng',
+    ],
+    [
+        'id' => 'danida',
+        'url' => 'http://um.dk/en',
+    ],
+    [
+        'id' => 'dutch-development-cooperation',
+        'url' => 'https://www.government.nl/topics/development-cooperation',
+    ],
+    [
+        'id' => 'formin-finland',
+        'url' => 'http://formin.finland.fi/public/default.aspx?culture=en-US&contentlan=2',
+    ],
+    [
+        'id' => 'giz',
+        'url' => 'https://www.giz.de/en/html/index.html',
+    ],
+    [
+        'id' => 'irish-aid',
+        'url' => 'https://www.irishaid.ie/#',
+    ],
+    [
+        'id' => 'luxdev',
+        'url' => 'https://luxdev.lu/en',
+    ],
+    [
+        'id' => 'mfa-norway',
+        'url' => 'https://www.regjeringen.no/en/dep/ud/id833/',
+    ],
+    [
+        'id' => 'mfa-poland',
+        'url' => 'http://formin.finland.fi/public/default.aspx?culture=en-US&contentlan=2',
+    ],
+    [
+        'id' => 'mfa-slovenia',
+        'url' => 'http://www.mzz.gov.si/en/foreign_policy_and_international_law/international_development_cooperation_and_humanitarian_assistance/international_development_cooperation_of_slovenia/',
+    ],
+    [
+        'id' => 'norad',
+        'url' => 'https://www.norad.no/en/',
+    ],
+    [
+        'id' => 'minfin-slovak-republic',
+        'url' => 'https://www.regjeringen.no/en/dep/ud/id833/',
+    ],
+    [
+        'id' => 'mfat-new-zealand',
+        'url' => 'https://www.mfat.govt.nz/en/aid-and-development/',
+    ],
+    [
+        'id' => 'mfa-poland',
+        'url' => '',
+    ],
+];
--- static/learn/related_pages.php
+++ static/learn/related_pages.php
@@ -8,7 +8,7 @@ return [
     [
         'id' => 'learn-courses',
         'label' => 'Courses',
-        'url' => Url::to(['/public/learn/courses']),
+        'url' => Url::to(['/s/courses']),
         'description' => 'A selection of online courses and training '
         . 'opportunities offered by our partners and other learning '
         . 'institutions.  Subscription based individual learning paths.',
@@ -16,7 +16,7 @@ return [
     [
         'id' => 'learn-publications',
         'label' => 'Publications',
-        'url' => Url::to(['/public/learn/publications']),
+        'url' => Url::to(['/s/publications']),
         'description' => 'A selection of published articles, videos and other '
         . 'online publications on diverse topics. Latest materials recommended '
         . 'by our expert groups. Resources to consult at all times.',
@@ -24,7 +24,7 @@ return [
     [
         'id' => 'learn-toolbox',
         'label' => 'Toolbox',
-        'url' => Url::to(['/public/learn/toolbox']),
+        'url' => Url::to(['/s/toolbox']),
         'description' => 'Dedicated to trainers. Improve your training skills '
         . 'with the latest developed tools and innovations in learning. Get '
         . 'familiar with with new training techniques and methods. Learn about '
