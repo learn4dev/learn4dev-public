5cb9121839f9a644e3186b5ebf5c78fcd854e348
jeffrey <jeffrey@10.100.13.74>
2018-02-26 16:17:05 +0100

    [MOD] Annual meeting page

--- views/about/annual-meeting.php
+++ views/about/annual-meeting.php
@@ -2,18 +2,18 @@
 <div id="layout-content">
     <div class="container">
         <?php ?>
-
+        <p>
+            <?php
+            $p = $this->theme->getBaseUrl() . '/img/l4d-save-the-date.png';
+            ?>
+            <img class="img-responsive" style="width: 50% ;margin:auto" src="<?= $p ?>" alt="Save the date! Annual Meeting 2018: 28th to 30th of May at ITCILO TURIN"> 
+        </p>
 
         <p style="margin:25px;text-align:center">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/lf5yxsn29ts" frameborder="0" allowfullscreen></iframe>
         </p>
 
-        <p>
 
-            <?php $p = $this->theme->getBaseUrl(). '/img/l4d-save-the-date.png';
-            ?>
-            <img class="img-responsive" style="width: 50% ;margin:auto" src="<?= $p ?>" alt="Save the date! Annual Meeting 2018: 28th to 30th of May at ITCILO TURIN"> 
-        </p>
     </div>
 
 
